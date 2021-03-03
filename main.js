b2d4ab140212f1f46014ea78f3f83ac701afb9f2
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-03 20:39:06 +0100

    Added data to main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const companies = [
+	{name: "Company One", category: "Finance", start: 1981, end: 2003},
+	{name: "Company Two", category: "Retail", start: 1991, end: 2008},
+	{name: "Company Three", category: "Auto", start: 1961, end: 2017},
+	{name: "Company Four", category: "Retail", start: 1983, end: 2001},
+	{name: "Company Five", category: "Technology", start: 1994, end: 2019},
+	{name: "Company Six", category: "Finance", start: 1999, end: 2021},
+	{name: "Company Seven", category: "Auto", start: 1956, end: 20018},
+	{name: "Company Eight", category: "Technology", start: 1947, end: 2015},
+	{name: "Company Nine", category: "Retail", start: 1972, end: 2009},
+];
+
+const ages = [33, 19, 20, 16, 25, 62, 32, 76, 43, 83, 34, 64, 46, 75, 18];
